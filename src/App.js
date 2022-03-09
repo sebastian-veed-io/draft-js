5f2822f21df7209dbf21eaa3c7223df870235b25
Sebastian Novak <sebastiannovak@Sebastians-MacBook-Pro.local>
2022-03-09 16:07:05 +0100

    test2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {
+  FC,
+  useCallback,
+  useEffect,
+  useState,
+  useRef,
+  useMemo,
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Editor, EditorState, RichUtils } from "draft-js";
 
 function App() {
+  const [editorState, setEditorState] = React.useState(EditorState.createEmpty()); 
+  const onChange = (state) => setEditorState(state);
+  const DummyRow = useMemo(props => {
+    <div><h1>BLOCK</h1><div {...props}></div></div>
+  });
+  const subtitleRowBlock = useMemo(
+    () => ({
+      component: DummyRow,
+    }),
+    [DummyRow],
+  );
+
+  const myBlockRenderer = useCallback(
+    (block) => {
+      return subtitleRowBlock;
+    },
+    [subtitleRowBlock],
+  );
+
   return (
     <div className="App">
+      <div className="editors">
+					<Editor
+						editorState={editorState}
+						onChange={onChange}
+            blockRendererFn={myBlockRenderer}
+					/>
+				</div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
